b885dda308b0b5ee45b0dbba742536cf2409cb02
begineer101 <abhijangra98@gmail.com>
2021-01-04 12:25:55 +0530

    Adding partials and layouts and settings up static files(css,js,images)

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+// extract style and script from sub page into layout
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(expressLayouts);
 app.use('/',require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
