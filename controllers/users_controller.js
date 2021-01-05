b00e0fde58e32b4f0bb694e43974b4a1927e3056
begineer101 <abhijangra98@gmail.com>
2021-01-05 15:06:25 +0530

    Adding authentication on user profile page (The user must be signed in to get to the profile page where the users data is shown)

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,22 @@
 const User = require('../models/user');
 
 module.exports.profile = function (req,res){
-    return res.render('users',{
-        title:'users page'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err, user){
+            if(err){console.log("Error in finding the userid"); return}
+
+            if(user){
+                return res.render('user_profile',{
+                    title:"User data",
+                    user: user
+                })
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+
 }
 
 module.exports.signIn = function (req,res){
@@ -43,5 +56,21 @@ module.exports.create = function (req,res){
 // creating session for user
 
 module.exports.createSession = function (req,res){
-    
+    User.findOne({email:req.body.email}, function(err, user){
+        if(err){console.log('error in finding Email of user'); return}
+
+        if(user){
+
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            return res.redirect('back');
+        }
+
+    });
 }
